e06ed47b68741800fc2f1eacbd05ff67a2b4c7ae
V-nyang <whitepony1214@gmail.com>
2023-07-02 22:45:48 +0900

    Fixed project settings (SQLite)

--- YomLog.MobileApp/Platforms/iOS/AppDelegate.cs
+++ YomLog.MobileApp/Platforms/iOS/AppDelegate.cs
@@ -1,5 +1,4 @@
 ﻿using Foundation;
-using SQLitePCL;
 
 namespace YomLog.MobileApp.Platforms.iOS;
 
@@ -8,7 +7,7 @@ public class AppDelegate : MauiUIApplicationDelegate
 {
     protected override MauiApp CreateMauiApp()
     {
-        raw.SetProvider(new SQLite3Provider_sqlite3());
+        SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_sqlite3());
         return MauiProgram.CreateMauiApp();
     }
 }
